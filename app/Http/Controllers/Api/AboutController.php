da765b0302a4a9f1c977d3ea780d0004fa58cb67
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2021-10-06 23:06:41 +0700

    api about

--- app/Http/Controllers/Api/AboutController.php
+++ app/Http/Controllers/Api/AboutController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\About\AboutCollection;
 use App\Models\About;
-use Illuminate\Http\Request;
 
 class AboutController extends Controller
 {
